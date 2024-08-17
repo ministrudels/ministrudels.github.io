901f7e9aef782cb7a107f943e0e7c0db395eb72b
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-08-17 10:13:15 +0100

    working chart

--- src/components/RustCrates/RustCrates.tsx
+++ src/components/RustCrates/RustCrates.tsx
@@ -5,7 +5,6 @@ import {
   Legend,
   Line,
   LineChart,
-  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -16,57 +15,11 @@ import { useGetDownloadTimeSeries } from "./utils";
 export default function RustCrates() {
   const { data, isLoading } = useGetDownloadTimeSeries("sqlx");
 
-  // // convert to chart data
-  // const chartData = data?.map((d) => ({
-  //   name: d.date,
-  //   pv: d.downloads,
-  //   uv: 2400,
-  // }));
+  const chartData = data?.map((d) => ({
+    name: d.date,
+    pv: d.downloads,
+  }));
 
-  const chartData = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
   return (
     <ExampleContainer
       title="Rust Crates"
@@ -77,36 +30,35 @@ export default function RustCrates() {
         <Grid item>
           <Typography variant="body1">
             It can be difficult to appraise crates in the Rust ecosystem. There
-            are many. This is a simple component to compare downloads.
+            are many. This is a simple component to compare key metrics when
+            appraising a crate.
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={300}
-              data={chartData}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
+          <LineChart
+            width={800}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
           <Typography variant="body1">
             {isLoading ? "Loading..." : "Loaded!"}
           </Typography>
