ff50b5b7351a82709663173bb349f3270503cbff
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-08-18 13:49:14 +0100

    refactor to own timeseries component

--- src/components/RustCrates/RustCrates.tsx
+++ src/components/RustCrates/RustCrates.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Divider, Grid, Typography } from "@mui/material";
+import { Box, Card, Grid, Typography } from "@mui/material";
 import {
   CartesianGrid,
   Label,
@@ -19,14 +19,58 @@ import CrateLogo from "./cargo.png";
 import { InputCrate } from "./InputCrate";
 import { useGetDownloadTimeSeries } from "./utils";
 
+const TimeSeries = ({ crates }: { crates: string[] }) => {
+  const { data } = useGetDownloadTimeSeries(crates);
+
+  const colourScale = getColourScale(crates.length);
+
+  return (
+    <Card variant="outlined" sx={{ width: "100%", padding: 2 }}>
+      {/* <ResponsiveContainer width="100%"> */}
+      <ResponsiveContainer width={"99%"} height={400}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="date"
+            tick={{ fontSize: 10 }}
+            tickFormatter={(value: string) => moment(value).format("MMM Do")}
+          />
+          <YAxis style={{ fontSize: 10 }}>
+            <Label angle={270} position="left" style={{ textAnchor: "middle" }}>
+              Total Downloads
+            </Label>
+          </YAxis>
+          <Tooltip />
+          <Legend />
+          {crates.map((crate, index) => (
+            <Line
+              key={crate}
+              type="monotone"
+              dataKey={crate}
+              stroke={colourScale(index)}
+              activeDot={{ r: 8 }}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
 export default function RustCrates() {
   const [crates, setCrates] = useState<string[]>([]);
-  const { data, isLoading } = useGetDownloadTimeSeries(crates);
 
   const handleDataFromChild = (data: string[]) => {
     setCrates(data);
   };
-  const colourScale = getColourScale(crates.length);
 
   return (
     <ExampleContainer
@@ -51,7 +95,6 @@ export default function RustCrates() {
             in the Rust ecosystem. There are many. This is a component to
             compare key metrics when appraising a crate.
           </Typography>
-          <Divider />
         </Grid>
         <Grid
           item
@@ -70,49 +113,7 @@ export default function RustCrates() {
               flexGrow: 1, // allows the item to grow and fill available space
             }}
           >
-            <Card variant="outlined" sx={{ width: "100%", padding: 2 }}>
-              {/* <ResponsiveContainer width="100%"> */}
-              <ResponsiveContainer width={"99%"} height={400}>
-                <LineChart
-                  data={data}
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis
-                    dataKey="date"
-                    tick={{ fontSize: 10 }}
-                    tickFormatter={(value: string) =>
-                      moment(value).format("MMM Do")
-                    }
-                  />
-                  <YAxis style={{ fontSize: 10 }}>
-                    <Label
-                      angle={270}
-                      position="left"
-                      style={{ textAnchor: "middle" }}
-                    >
-                      Total Downloads
-                    </Label>
-                  </YAxis>
-                  <Tooltip />
-                  <Legend />
-                  {crates.map((crate, index) => (
-                    <Line
-                      key={crate}
-                      type="monotone"
-                      dataKey={crate}
-                      stroke={colourScale(index)}
-                      activeDot={{ r: 8 }}
-                    />
-                  ))}
-                </LineChart>
-              </ResponsiveContainer>
-            </Card>
+            <TimeSeries crates={crates} />
           </Grid>
         </Grid>
       </Grid>
