008341c8bdca6ddeb584e49f4dcc794d165c703a
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-08-18 13:45:11 +0100

    responsive container

--- src/components/RustCrates/RustCrates.tsx
+++ src/components/RustCrates/RustCrates.tsx
@@ -5,6 +5,7 @@ import {
   Legend,
   Line,
   LineChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -28,7 +29,11 @@ export default function RustCrates() {
   const colourScale = getColourScale(crates.length);
 
   return (
-    <ExampleContainer title="Rust Crates" date={new Date("8 17 2024")}>
+    <ExampleContainer
+      title="Rust Crates"
+      date={new Date("8 17 2024")}
+      tags={["recharts", "custom hooks", "rust"]}
+    >
       <Grid container spacing={2} alignItems="center">
         <Grid item>
           <Typography variant="body1">
@@ -48,52 +53,65 @@ export default function RustCrates() {
           </Typography>
           <Divider />
         </Grid>
-        <Grid item container alignItems={"flex-start"}>
+        <Grid
+          item
+          container
+          alignItems={"flex-start"}
+          sx={{
+            width: "100%",
+          }}
+        >
           <Grid item xs={2}>
             <InputCrate onCrateChange={handleDataFromChild} />
           </Grid>
-          <Grid item>
-            <Card variant="outlined" sx={{ padding: 2 }}>
-              <LineChart
-                width={600}
-                height={400}
-                data={data}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="date"
-                  tick={{ fontSize: 10 }}
-                  tickFormatter={(value: string) =>
-                    moment(value).format("MMM Do")
-                  }
-                />
-                <YAxis style={{ fontSize: 10 }}>
-                  <Label
-                    angle={270}
-                    position="left"
-                    style={{ textAnchor: "middle" }}
-                  >
-                    Total Downloads
-                  </Label>
-                </YAxis>
-                <Tooltip />
-                <Legend />
-                {crates.map((crate, index) => (
-                  <Line
-                    key={crate}
-                    type="monotone"
-                    dataKey={crate}
-                    stroke={colourScale(index)}
-                    activeDot={{ r: 8 }}
+          <Grid
+            item
+            sx={{
+              flexGrow: 1, // allows the item to grow and fill available space
+            }}
+          >
+            <Card variant="outlined" sx={{ width: "100%", padding: 2 }}>
+              {/* <ResponsiveContainer width="100%"> */}
+              <ResponsiveContainer width={"99%"} height={400}>
+                <LineChart
+                  data={data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis
+                    dataKey="date"
+                    tick={{ fontSize: 10 }}
+                    tickFormatter={(value: string) =>
+                      moment(value).format("MMM Do")
+                    }
                   />
-                ))}
-              </LineChart>
+                  <YAxis style={{ fontSize: 10 }}>
+                    <Label
+                      angle={270}
+                      position="left"
+                      style={{ textAnchor: "middle" }}
+                    >
+                      Total Downloads
+                    </Label>
+                  </YAxis>
+                  <Tooltip />
+                  <Legend />
+                  {crates.map((crate, index) => (
+                    <Line
+                      key={crate}
+                      type="monotone"
+                      dataKey={crate}
+                      stroke={colourScale(index)}
+                      activeDot={{ r: 8 }}
+                    />
+                  ))}
+                </LineChart>
+              </ResponsiveContainer>
             </Card>
           </Grid>
         </Grid>
